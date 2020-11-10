21a1fe333ffa93deb27563a6f912b811cd3fb57c
Nick <lacoste777v@gmail.com>
2020-11-10 16:18:09 +0300

    add slide change

--- src/script/index.js
+++ src/script/index.js
@@ -1,27 +1,6 @@
-let slideIndex = 0;
-showSlides();
-   
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slideDenali");
-    for (i = 0; i < slides.length; i+=1) {
-        slides[i].style.display = "none";
-    }
-slideIndex+=1;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000);
-}
-
-
-
-
 function menuHeaderBehavior() {
     let oldScroll = 0
     let heroHeader = document.querySelector('.header')
-    let heroHeaderHeight = heroHeader.clientHeight
     window.addEventListener('scroll', () => {
         let currentScroll = window.pageYOffset
         if(currentScroll > oldScroll ) {
@@ -38,15 +17,64 @@ menuHeaderBehavior()
 
 
 
+// let slideIndex = 0;
+// showSlides();
 
-function qwe() {
-    let q = document.getElementById("test");
-    q.style.display="block";    
-}
 
-let butt=document.getElementById("butt");
+// function showSlides() {
+//     let i;
+//     let slides = document.getElementsByClassName("slideWildlife");
+//     for (i = 0; i < slides.length; i+=1) {
+//         slides[i].style.display = "none";
+//     }
+// slideIndex+=1;
+//     if (slideIndex > slides.length) {
+//         slideIndex = 1;
+// }
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showSlides, 3000);
+// }
 
-butt.addEventListener("click", qwe);
 
+// function showAuroras() {
+//     let i;
+//     let slideZero=document.getElementsByClassName("slideDenali");
+//     slideZero.style.display = "none";
+//     let slides = document.getElementsByClassName("slideAurora");
+//     for (i = 0; i < slides.length; i+=1) {
+//         slides[i].style.display = "none";
+//     }
+// slideIndex+=1;
+//     if (slideIndex > slides.length) {
+//         slideIndex = 1;
+// }
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showAuroras, 3000);
+// }
+// let butt = document.getElementById("asx");
+// butt.addEventListener("click", showAuroras)
+
+
+
+let slideIndex = 0;
+
+showSlides("slideDenali");
+
+function showSlides(name) {
+        let i;
+        let slides = document.getElementsByClassName(name);
+        for (i = 0; i < slides.length; i+=1) {
+            slides[i].style.display = "none";
+        }
+    slideIndex+=1;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+    }
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 3000);
+    }
+    
+
+  
 
 
